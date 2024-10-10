f415a4b7256adad8c0cb26b82e0244dccb8d67b8
stalin670 <amitalvin01@gmail.com>
2024-10-10 22:32:45 +0530

    Created register api controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,28 @@
-export const register = async () => {};
-export const login = async () => {};
-export const authStatus = async () => {};
-export const logout = async () => {};
-export const setup2FA = async () => {};
-export const verify2FA = async () => {};
-export const reset2FA = async () => {};
+import bcrypt from "bcryptjs";
+import User from "../models/user.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      isMfaActive: false,
+    });
+
+    console.log("New User : ", newUser);
+    await newUser.save();
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Error registering user", message: error });
+  }
+};
+export const login = async (req, res) => {};
+export const authStatus = async (req, res) => {};
+export const logout = async (req, res) => {};
+export const setup2FA = async (req, res) => {};
+export const verify2FA = async (req, res) => {};
+export const reset2FA = async (req, res) => {};
